d7d3843a3db70d29ed32ff68595c7c15530ee787
pokal4u <pokalasrinu@gmail.com>
2023-09-15 10:35:25 +0530

    Add files via upload

---
+++ configabc.py
@@ -0,0 +1,32 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 # Fake AWS credentials\
+aws_access_key_id = AKIAIOSFODNN7EXAMPLE\
+aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY\
+\
+# Fake Azure AD key\
+AZURE_AD_KEY=MC49D-PP2W4-YR4WY-VV7GB-7C2KP\
+\
+# Fake RSA private key\
+-----BEGIN RSA PRIVATE KEY-----\
+MIICWwIBAAKBgQCqGKukO1De7zhZj6+H0qtjTkVxwTCpvKe4eCZ0FPqri0cb2JZfX\
+JGtqd2Z9CIQxyw3XF/mq8Y1vQY37Dhw2RvkAxrQAOY8zFfYlF1G1eOlqQ5nEK1u5\
+...\
+-----END RSA PRIVATE KEY-----\
+\
+# Fake API key\
+API_KEY=abcd1234deadbeef87654321\
+\
+# Fake database connection string\
+DATABASE_URL=postgres://username:password@hostname:5432/mydatabase\
+\
+# Fake OAuth token\
+oauth_token=19827398127398127398127398127398\
+\
+# Other fake secrets...\
+}
